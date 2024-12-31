1bf66091efd1a15e274e378b3e655eeaf6bf622e
AHuyHarryIT <ahgamingofficial03@gmail.com>
2024-12-31 23:02:09 +0700

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
+      <h1 className="text-center text-4xl font-bold">Hello GIT</h1>
+    </div>
+  );
+}
+
+export default App;
