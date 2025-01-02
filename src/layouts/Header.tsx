32a6f2b09edbe4de38fd0f4a33fbb1e63ce19ae3
Nguyễn Anh Huy <85728869+AHuyHarryIT@users.noreply.github.com>
2025-01-02 23:27:24 +0700

    feat: header layout (#3)

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,26 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo from "@/assets/logo.png";
+
+const routeLinks = [
+  { to: "/", label: "Home" },
+  { to: "/events", label: "Events" },
+  { to: "/blogs", label: "Blogs" },
+  { to: "/projects", label: "Projects" },
+  { to: "/about", label: "About us" },
+];
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/events">Event</Link>
-        </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/about">About us</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex items-center justify-between px-4 py-2 shadow-lg">
+      <Link to="/">
+        <img src={logo} alt="logo" width={32} height={32} />
+      </Link>
+      <nav>
+        <ul className="flex space-x-4">
+          {routeLinks.map((items, index) => (
+            <li key={index + items.to}>
+              <NavLink
+                to={items.to}
+                className={({ isActive }) =>
+                  isActive ? "font-semibold text-blue-700" : ""
+                }
+              >
+                {items.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
